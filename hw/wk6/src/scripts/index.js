7784c6496cb9a5d84eef0b8258278b7019304da1
菜鳥祐 <a9650615@gmail.com>
2017-03-30 02:10:59 +0800

    wk6 基本完成

---
+++ hw/wk6/src/scripts/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+import 'babel-polyfill';
+import '../styles/index.scss';
+import '../styles/normalize.css';
+import '../styles/skeleton.css';
